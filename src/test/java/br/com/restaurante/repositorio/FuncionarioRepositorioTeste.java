54d1603af9ab56381c9b173ef4d3491562619d81
Marcelo Campos Silva <marcelo.home19@gmail.com>
2019-10-29 12:24:48 -0300

    :white_check_mark: Criado Testes FuncionarioRepositorio

---
+++ src/test/java/br/com/restaurante/repositorio/FuncionarioRepositorioTeste.java
@@ -0,0 +1,86 @@
+package br.com.restaurante.repositorio;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.restaurante.RestauranteApplication;
+import br.com.restaurante.modelo.Funcionario;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RestauranteApplication.class)
+public class FuncionarioRepositorioTeste {
+	
+	@Autowired
+	private FuncionarioRepositorio funcionarioRepositorio;
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	private Funcionario funcionarioNovo;
+	
+	@Before
+	public void antes() {
+		funcionarioNovo = new Funcionario();
+		funcionarioNovo.setNome("Wiliam");
+		funcionarioNovo.setEmail("wiliam@email.com.br");
+		funcionarioNovo.setVoto(false);		
+		this.funcionarioRepositorio.saveAndFlush(funcionarioNovo);
+	}
+	
+	@After
+	public void depois() {
+		this.funcionarioRepositorio.deleteAll();
+	}
+	
+	@Test
+	public void criarFuncionarioSucesso() throws Exception {			
+		assertThat(funcionarioNovo.getId()).isNotNull();
+		assertThat(funcionarioNovo.getNome()).isEqualTo("Wiliam");
+		assertThat(funcionarioNovo.getEmail()).isEqualTo("wiliam@email.com.br");
+		
+	}
+	
+	@Test
+	public void criarFuncionarioNomeErro() throws Exception {	
+		thrown.expect(ConstraintViolationException.class);
+		thrown.expectMessage("O campo nome é obrigatório.");
+		this.funcionarioRepositorio.saveAndFlush(new Funcionario("","teste@email.com",false));		
+	}
+	
+	@Test
+	public void criarFuncionarioEmailErro() throws Exception {	
+		thrown.expect(ConstraintViolationException.class);
+		thrown.expectMessage("O campo e-mail é obrigatório.");
+		this.funcionarioRepositorio.saveAndFlush(new Funcionario("Teste","",false));		
+	}	
+	
+	@Test
+	public void buscarFuncionarioPorEmailSucesso() throws Exception {		
+		Funcionario busca = funcionarioRepositorio.findByEmail(funcionarioNovo.getEmail());		
+		assertThat(busca.getEmail()).isEqualTo("wiliam@email.com.br");			
+	}
+	
+	@Test
+	public void buscarFuncionarioPorEmailErro() throws Exception {		
+		assertThat(funcionarioRepositorio.findByEmail("erro@email.com.br")).isNull();			
+	}		
+	
+	@Test
+	public void removerFuncionario() throws Exception {
+		this.funcionarioRepositorio.delete(funcionarioNovo);			
+		assertThat(funcionarioRepositorio.findByEmail("wiliam@email.com.br")).isNull();
+	}
+	
+
+}
