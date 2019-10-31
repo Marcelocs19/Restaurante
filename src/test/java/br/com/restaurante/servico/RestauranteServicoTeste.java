9cbed89eb65def866f21d2d7f8c002814749b20f
Marcelo Campos Silva <marcelo.home19@gmail.com>
2019-10-31 13:49:12 -0300

    :white_check_mark: Teste votar, limpar os restaurantes que já foram votados na semana

--- src/test/java/br/com/restaurante/servico/RestauranteServicoTeste.java
+++ src/test/java/br/com/restaurante/servico/RestauranteServicoTeste.java
@@ -57,6 +57,10 @@ public class RestauranteServicoTeste {
 	private Restaurante restaurante2;
 	private Restaurante restaurante3;
 	private Restaurante restaurante4;
+	private Restaurante restaurante5;
+	private Restaurante restaurante6;
+	private Restaurante restaurante7;
+	private Restaurante restaurante8;
 
 	private Votacao votacao1;
 
@@ -97,15 +101,43 @@ public class RestauranteServicoTeste {
 		restaurante4.setNome("Silva Lanches");
 		restaurante4.setDataVitoria(dataVitoria);
 		restaurante4.setEstado(INDISPONIVEL);
+		
+		LocalDate dataVitoria2 = LocalDate.of(2019, 10, 29);
+		restaurante5 = new Restaurante();
+		restaurante5.setId(9L);
+		restaurante5.setNome("Servero Hamburgueria");
+		restaurante5.setDataVitoria(dataVitoria2);
+		restaurante5.setEstado(INDISPONIVEL);
+		
+		LocalDate dataVitoria3 = LocalDate.of(2019, 10, 30);
+		restaurante6 = new Restaurante();
+		restaurante6.setId(10L);
+		restaurante6.setNome("Restaurante Vitória");
+		restaurante6.setDataVitoria(dataVitoria3);
+		restaurante6.setEstado(INDISPONIVEL);
+		
+		LocalDate dataVitoria4 = LocalDate.of(2019, 11, 1);
+		restaurante7 = new Restaurante();
+		restaurante7.setId(11L);
+		restaurante7.setNome("Espaço32");
+		restaurante7.setDataVitoria(dataVitoria4);
+		restaurante7.setEstado(INDISPONIVEL);
+		
+		LocalDate dataVitoria5 = LocalDate.of(2019, 11, 4);
+		restaurante8 = new Restaurante();
+		restaurante8.setId(12L);
+		restaurante8.setNome("Mazá");
+		restaurante8.setDataVitoria(dataVitoria5);
+		restaurante8.setEstado(INDISPONIVEL);
 
 		funcionario1 = new Funcionario();
-		funcionario1.setId(5L);
+		funcionario1.setId(10L);
 		funcionario1.setEmail("teste@email.com");
 		funcionario1.setNome("Teste");
 		funcionario1.setVoto(false);
 
 		funcionario2 = new Funcionario();
-		funcionario2.setId(6L);
+		funcionario2.setId(11L);
 		funcionario2.setEmail("teste2@email.com");
 		funcionario2.setNome("Teste2");
 		funcionario2.setVoto(false);
@@ -181,7 +213,7 @@ public class RestauranteServicoTeste {
 		funcionarioForm.setNome("Teste");
 		when(funcionarioRepositorio.findByEmail("teste@email.com")).thenReturn(funcionario1);
 
-		List<RestauranteDto> listaRestaurantesVoto = restauranteServico.votar(10L, funcionarioForm);
+		List<RestauranteDto> listaRestaurantesVoto = restauranteServico.votar(20L, funcionarioForm);
 
 		assertThat(listaRestaurantesVoto.get(0).getNome()).isEqualTo("Pizzaria Fragata");
 		assertThat(listaRestaurantesVoto.get(1).getNome()).isEqualTo("Restaurante Panorama");
@@ -216,6 +248,52 @@ public class RestauranteServicoTeste {
 		assertThat(listaRestaurantesVoto.get(0).getNome()).isEqualTo("Pizzaria Fragata");
 		assertThat(listaRestaurantesVoto.get(1).getNome()).isEqualTo("Restaurante Panorama");
 		assertThat(listaRestaurantesVoto.get(2).getNome()).isEqualTo("Churrascaria Freio de Ouro");
+	}
+	
+	@Test
+	public void testeVotarRestauranteLimparListaRestaurante() throws Exception {
+		Optional<Restaurante> restaurante = Optional.empty();
+		restaurante = Optional.of(restaurante2);
+		when(restauranteRepositorioMock.findById(restaurante2.getId())).thenReturn(restaurante);
+		
+		restaurante1.setNumeroVotos(0);
+		restaurante2.setNumeroVotos(0);
+		restaurante3.setNumeroVotos(0);
+		listaRestauranteDisponiveis.addAll(Arrays.asList(restaurante2, restaurante1, restaurante3
+				,restaurante4,restaurante5,restaurante6,restaurante7,restaurante8));
+		when(restauranteRepositorioMock.findAllByOrderByNumeroVotosDesc()).thenReturn(listaRestauranteDisponiveis);
+		
+		listaRestauranteIndisponiveis.addAll(Arrays.asList(restaurante4,restaurante5,restaurante6,restaurante7,restaurante8));
+		when(restauranteRepositorioMock.findByEstado(INDISPONIVEL)).thenReturn(listaRestauranteIndisponiveis);
+		
+		listaRestauranteVitoria.addAll(Arrays.asList(restaurante8,restaurante7,restaurante6,restaurante5,restaurante4));
+		when(restauranteRepositorioMock.findAllByOrderByDataVitoriaDesc()).thenReturn(listaRestauranteVitoria);
+		
+		listaFuncionario.addAll(Arrays.asList(funcionario1, funcionario2));
+		when(funcionarioRepositorio.findByVoto(false)).thenReturn(listaFuncionario);
+		
+		Funcionario funcionario4 = new Funcionario();
+		funcionario4.setEmail("teste4@email.com");
+		funcionario4.setNome("Teste4");
+		funcionario4.setVoto(false);
+		funcionario4.setId(12L);
+		
+		FuncionarioForm funcionarioForm = new FuncionarioForm();
+		funcionarioForm.setEmail("teste4@email.com");
+		funcionarioForm.setNome("Teste4");
+		when(funcionarioRepositorio.findByEmail("teste4@email.com")).thenReturn(funcionario4);
+		
+		List<RestauranteDto> listaRestaurantesVoto = restauranteServico.votar(2L, funcionarioForm);
+		
+		assertThat(listaRestaurantesVoto.get(0).getNome()).isEqualTo("Churrascaria Freio de Ouro");
+		assertThat(listaRestaurantesVoto.get(1).getNome()).isEqualTo("Pizzaria Fragata");		
+		assertThat(listaRestaurantesVoto.get(2).getNome()).isEqualTo("Restaurante Panorama");		
+		assertThat(listaRestaurantesVoto.get(3).getNome()).isEqualTo("Silva Lanches");
+		assertThat(listaRestaurantesVoto.get(4).getNome()).isEqualTo("Servero Hamburgueria");
+		assertThat(listaRestaurantesVoto.get(5).getNome()).isEqualTo("Restaurante Vitória");
+		assertThat(listaRestaurantesVoto.get(6).getNome()).isEqualTo("Espaço32");
+		assertThat(listaRestaurantesVoto.get(7).getNome()).isEqualTo("Mazá");
+		
 	}
 	
 }
